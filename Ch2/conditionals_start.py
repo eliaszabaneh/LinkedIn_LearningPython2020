9cfd1abc428ac3e9cf93792efc76798afb18cb51
eliaszabaneh <elias.zabaneh@gmail.com>
2022-03-09 17:45:18 +0400

    Initial commit

---
+++ Ch2/conditionals_start.py
@@ -0,0 +1,22 @@
+#
+# Example file for working with conditional statements
+#
+
+
+def main():
+    x, y = 1000, 1000
+
+    # conditional flow uses if, elif, else
+    if x < y:
+        st = "x is less than y"
+    elif x == y:
+        st = "x is the same as y"
+    else:
+        st = "x is greater than y"
+
+    print(st)
+# conditional statements let you use "a if C else b"
+
+
+if __name__ == "__main__":
+    main()
